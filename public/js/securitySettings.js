d0cbdaa5a655d4c3d057353a92257389b8066bd6
Lucas Lund <lucas@Lucass-MacBook-Pro.local>
2018-09-07 09:28:16 +0200

    Fixed delete account button, added more user info on profile page

---
+++ public/js/securitySettings.js
@@ -0,0 +1,20 @@
+function test() {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover your account!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+            var form = document.createElement('form');
+            document.body.appendChild(form);
+            form.method = 'post';
+            form.action = '/user/settings/security/DeleteAccount';
+            form.submit();
+        } else {
+          swal("Account has not been deleted");
+        }
+      });
+}
