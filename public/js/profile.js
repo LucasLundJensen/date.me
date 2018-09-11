cbda92df7fe973e045e57fe963b89ae2877ae8ff
Lucas Lund Jensen <lucas.lund@live.dk>
2018-09-11 13:24:14 +0200

    Added "joined X days ago" to profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,3 +2,14 @@ $( '#editBio' ).on('click', function() {
     $( '.bioForm' ).toggleClass('visible');
     $( '#user_bio' ).toggleClass('invisible');
 })
+
+var joinDateElement = document.getElementById("joinDate"),
+    jDate = joinDateElement.dataset.parent,
+    today = new Date().toJSON().slice(0,10).replace(/-/g,'-'),
+    milliseconds = (Date.parse(today) - Date.parse(jDate)),
+    seconds = parseInt(Math.floor(milliseconds / 1000)),
+    minutes = parseInt(Math.floor(seconds / 60)),
+    hours = parseInt(Math.floor(minutes / 60)),
+    days = parseInt(Math.floor(hours / 24))
+
+joinDateElement.innerHTML = "Joined " + days + " days ago";
